3ac11caa91e946bb76ce957684d5a3c20e50319c
dfitriana <diyahayufa@students.unnes.ac.id>
2022-05-26 18:32:17 +0700

    update home

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Layout from "../components/layout/Layout";
+import Hero from "../components/homepage/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        <Hero />
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi at consequatur similique neque error sunt quibusdam velit asperiores sit, iste aspernatur eaque delectus id, repellat dolorem possimus quo. Architecto, ullam!
+      </Layout>
+    </div>
+  );
+}
