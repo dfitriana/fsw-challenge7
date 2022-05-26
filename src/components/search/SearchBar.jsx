6f039fa4e1a3809a2c05442519f47b23f4678b9d
dfitriana <diyahayufa@students.unnes.ac.id>
2022-05-26 21:41:51 +0700

    update card

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -62,6 +62,8 @@ export default function SearchBar() {
         </Button>
       </div>
     </Container>
+
+    
     
 
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
 import Hero from "../components/homepage/Hero";
 import SearchBar from "../components/search/SearchBar";
+import CardResult from "../components/card/CardResult";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
@@ -9,6 +11,9 @@ export default function Home() {
       <Layout>
         <Hero />
         <SearchBar />
+        <Container>
+          <CardResult />
+        </Container>
       </Layout>
     </div>
   );
