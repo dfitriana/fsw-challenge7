d43644eec22d561d55b6842eb5da00110f0d93ca
dfitriana <diyahayufa@students.unnes.ac.id>
2022-05-27 11:32:05 +0700

    update

---
+++ src/pages/cars.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+export default function Search() {
+    // const router = useRouter();
+
+  return (
+    <>
+      <Layout>
+dddkkkk
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolore quidem laudantium? Sint accusamus repellat dolores numquam qui debitis. Architecto nisi ipsa blanditiis porro optio veniam delectus id quisquam labore.
+      </Layout>
+    </>
+  );
+}
