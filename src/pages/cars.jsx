084e5faf43abe053fe2f0f01c79759f718ecd4ac
dfitriana <diyahayufa@students.unnes.ac.id>
2022-05-27 14:05:39 +0700

    Update cars.jsx

--- src/pages/cars.jsx
+++ src/pages/cars.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/layout/Layout";
+import Hero from "../components/homepage/Hero";
+import SearchBar from "../components/search/SearchBar";
+import CardResult from "../components/card/CardResult";
+import { Container } from "react-bootstrap";
 
 export default function Cars() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Layout>
+        <Hero />
+        <SearchBar />
+        <Container>
+          <CardResult />
+        </Container>
+      </Layout>
+    </>
+  );
 }
